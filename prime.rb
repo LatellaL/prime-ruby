4463a02e0796ec699ece65e07bc31b8f568c3bd8
LatellaL <83371530+LatellaL@users.noreply.github.com>
2021-10-04 20:09:25 -0400

    commit tree 251c184930f4256360472547ac788dfc0dcb3e0c

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+# prime? returns true for prime numbers
+def prime?(number)
+    start = 2
+    if number > 1
+        range = (start..number-1).to_a
+        range.none? do |num_to_test|
+            number% num_to_test == 0
+        end
+# prime? returns false for non-prime numbers
+    else 
+        false
+    end 
+end
